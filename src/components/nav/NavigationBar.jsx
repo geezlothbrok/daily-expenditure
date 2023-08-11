914e3a3d818e729fe1bbbd33b24231ed173a460b
geezlothbrok <giddyohene5@gmail.com>
2023-08-11 01:36:13 +0000

    clear user details when a user is logged out from redux state

--- src/components/nav/NavigationBar.jsx
+++ src/components/nav/NavigationBar.jsx
@@ -1,120 +1,128 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { HiMenuAlt2, HiUserCircle } from "react-icons/hi";
 import { RxCross1 } from "react-icons/rx";
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from "react-router-dom";
 import "./NavigationBar.css";
 import { onAuthStateChanged, signOut } from "firebase/auth";
-import { auth } from '../../firebase/config';
-import { toast } from 'react-toastify';
-import { useDispatch } from 'react-redux';
-import { SET_ACTIVE_USER } from '../../redux/slice/authSlice';
+import { auth } from "../../firebase/config";
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import {
+  REMOVE_ACTIVE_USER,
+  SET_ACTIVE_USER,
+} from "../../redux/slice/authSlice";
 
 function NavigationBar() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [displayName, setDisplayName] = useState("");
 
-    const [showMenu, setShowMenu] = useState(false)
-    const [displayName, setDisplayName] = useState("")
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+  const hideMenu = () => {
+    setShowMenu(false);
+  };
 
-    const toggleMenu = () => {
-      setShowMenu(!showMenu)
-    }
-    const hideMenu = () => {
-      setShowMenu(false)
-    }
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-
-    const logo = (
-        <div className="logo">
+  const logo = (
+    <div className="logo">
       <p className="logo-text">Expenses Manager</p>
     </div>
-    );
+  );
 
-    const activeLink = ({isActive}) => (isActive ? "active" : "");
-    const navigate = useNavigate();
+  const activeLink = ({ isActive }) => (isActive ? "active" : "");
+  const navigate = useNavigate();
 
-
-      // LOG A USER OUT
-    const logoutUser = () => {
-      signOut(auth).then(() => {
-       toast.success("Logot successful");
-       navigate("/login");
-      }).catch((error) => {
-       toast.error(error.message)
+  // LOG A USER OUT
+  const logoutUser = () => {
+    signOut(auth)
+      .then(() => {
+        toast.success("Logot successful");
+        navigate("/login");
+      })
+      .catch((error) => {
+        toast.error(error.message);
       });
-    };
+  };
 
+  // MONITOR CURRENTLY SIGNED IN USER
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid;
+        // console.log(user.displayName);
 
-    // MONITOR CURRENTLY SIGNED IN USER
-    useEffect(() => {
-      onAuthStateChanged(auth, (user) => {
-        if (user) {
-          const uid = user.uid;
-          // console.log(user.displayName);
-
-          if (user.displayName == null) {
-            const u1 = user.email.substring(0, user.email.indexOf("@"));
-            const uName = u1.charAt(0).toUpperCase() + u1.slice(1);
-            setDisplayName(uName)
-          } else {
-            setDisplayName(user.displayName);
-          }
-
-          
-          dispatch(SET_ACTIVE_USER({
+        if (user.displayName == null) {
+          const u1 = user.email.substring(0, user.email.indexOf("@"));
+          const uName = u1.charAt(0).toUpperCase() + u1.slice(1);
+          setDisplayName(uName);
+        } else {
+          setDisplayName(user.displayName);
+        }
+        dispatch(
+          SET_ACTIVE_USER({
             email: user.email,
             userName: user.displayName ? user.displayName : displayName,
             userID: user.uid,
-          }));
-         
-        } else {
-          setDisplayName("")
-        }
-      });
-      
-    }, [])
-    
+          })
+        );
+      } else {
+        setDisplayName("");
+        dispatch(REMOVE_ACTIVE_USER());
+      }
+    });
+  }, []);
 
   return (
     <>
-
-    <nav className="container navbar">
+      <nav className="container navbar">
         {logo}
 
-    <menu>
-      <ul className="nav-links fixed" id={showMenu ? "mobile-nav-links" : "hide-mobile-nav-links"}>
-        
-        <li className="user-icon"  onClick={hideMenu}>
-          <HiUserCircle color="white" size={25} />
-          <span>Hi, {displayName}</span>
-        </li>
+        <menu>
+          <ul
+            className="nav-links fixed"
+            id={showMenu ? "mobile-nav-links" : "hide-mobile-nav-links"}
+          >
+            <li className="user-icon" onClick={hideMenu}>
+              <HiUserCircle color="white" size={25} />
+              <span>Hi, {displayName}</span>
+            </li>
 
-        <li onClick={hideMenu}>
-          <NavLink to="/login" className={activeLink}>Login</NavLink>
-        </li>
-        
-        <li onClick={hideMenu}>
-          <NavLink to ="/addExpense" className="btn-orange">Add Expense</NavLink>
-        </li>
-        <li onClick={hideMenu}>
-          <NavLink to="/register" className={activeLink}>Register</NavLink>
-        </li>
-        <li onClick={hideMenu}>
-          <NavLink to="/login" onClick={logoutUser}>Logout</NavLink>
-        </li>
-      </ul>
-    </menu>
+            <li onClick={hideMenu}>
+              <NavLink to="/login" className={activeLink}>
+                Login
+              </NavLink>
+            </li>
 
-    <div className="menu-icons" onClick={toggleMenu}>
-      {showMenu ? (
-        <RxCross1 color="white" size={23} />
-      ) : (
-        <HiMenuAlt2 color="white" size={23} />
-      )}
-      
-    </div>
-  </nav>
-  </>
-  )
+            <li onClick={hideMenu}>
+              <NavLink to="/addExpense" className="btn-orange">
+                Add Expense
+              </NavLink>
+            </li>
+            <li onClick={hideMenu}>
+              <NavLink to="/register" className={activeLink}>
+                Register
+              </NavLink>
+            </li>
+            <li onClick={hideMenu}>
+              <NavLink to="/login" onClick={logoutUser}>
+                Logout
+              </NavLink>
+            </li>
+          </ul>
+        </menu>
+
+        <div className="menu-icons" onClick={toggleMenu}>
+          {showMenu ? (
+            <RxCross1 color="white" size={23} />
+          ) : (
+            <HiMenuAlt2 color="white" size={23} />
+          )}
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default NavigationBar
+export default NavigationBar;
--- src/redux/slice/authSlice.js
+++ src/redux/slice/authSlice.js
@@ -19,11 +19,17 @@ const authSlice = createSlice({
             state.userName = userName
             state.userID = userID
 
-        }
+        },
+        REMOVE_ACTIVE_USER:  (state, actions) => {
+            state.isLoggedIn = false;
+            state.email = null
+            state.userName = null
+            state.userID = null
+        },
     }
 });
 
-export const {SET_ACTIVE_USER} = authSlice.actions;
+export const {SET_ACTIVE_USER, REMOVE_ACTIVE_USER} = authSlice.actions;
 
 export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
 
