1385f01c1fb70364850ce0c1384b08a46d859997
geezlothbrok <giddyohene5@gmail.com>
2023-08-19 21:31:04 +0000

    debugging

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import FetchData from '../components/FetchData';
-import "./Home.css"
+
 
 function Home() {
   return (
     <>
     <div className='expenseListContainer'>
       <h1 className="headText">LIST OF ALL EXPENSES</h1>
-        <FetchData/>
+       hello
     </div>
     </>
   )
